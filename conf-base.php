de985e3d76bf1e13476275cb0bf603d56d5e944b
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-12-11 16:06:05 -0200

    adiciona conf-base para desabilitar as entidades agente, projeto e evento

---
+++ conf-base.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'app.enabled.agents'   => false,
+    'app.enabled.projects' => false,
+    'app.enabled.events'   => false,
+    'app.enabled.apps'     => false,
+    
+    'snbp.agentId' => 1,
+];
