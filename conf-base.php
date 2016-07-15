5651dc7eb3a32a390c2ee6fa205b46f70a83adbd
Marylly Araújo Silva <marylly@hacklab.com.br>
2016-07-15 01:11:21 -0300

    Removing seal configuration.

--- conf-base.php
+++ conf-base.php
@@ -4,7 +4,5 @@ return [
     'app.enabled.projects' => false,
     'app.enabled.events'   => false,
     'app.enabled.apps'     => false,
-    'app.enabled.seals'    => false,
-
-    'museus.ownerAgentId'   => 11348,
+    'museus.ownerAgentId'   => 1
 ];
