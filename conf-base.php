3bf447a20d95c63ad9b0737bbed41aa9338cff59
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2016-12-15 10:41:36 -0200

    Enable Events and Projects entities

--- conf-base.php
+++ conf-base.php
@@ -2,11 +2,12 @@
 
 return [
     'app.enabled.agents'            => false,
-    'app.enabled.projects'          => false,
-    'app.enabled.events'            => false,
+    'app.enabled.projects'          => true,
+    'app.enabled.events'            => true,
     'app.enabled.apps'              => false,
     'app.enabled.seals'             => true,
     'museus.ownerAgentId'           => 11348,
-    'notifications.entities.update' => 1825,
+    // 'notifications.entities.update' => 1825,
+    'notifications.entities.update' => 0,
     'notifications.user.access'     => 182
 ];
