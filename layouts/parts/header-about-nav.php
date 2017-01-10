dde5c6842f94b50030f2fe15b5f880941076854e
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2017-01-10 18:21:02 -0200

    Add custom  about page

---
+++ layouts/parts/header-about-nav.php
@@ -0,0 +1,5 @@
+<nav id="about-nav" class="alignright clearfix">
+    <ul id="secondary-menu">
+        <li><a class="icon icon-about hltip" href="<?php echo $app->createUrl('site', 'page', array('sobre')) ?>" title="Sobre"></a></li>
+    </ul>
+</nav>
