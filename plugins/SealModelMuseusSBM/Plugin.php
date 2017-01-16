dfdbbba0bb9c510fd7f84dc110954ae92d263d34
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2017-01-16 11:38:46 -0200

    Add custom Seal Model

---
+++ plugins/SealModelMuseusSBM/Plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SealModelMuseusSBM;
+
+use SealModelTab;
+
+
+
+class Plugin extends SealModelTab\SealModelTemplatePlugin
+{
+    function getModelData(){
+        return [
+            'label'=> 'Modelo Museus (SBM)',
+            'name' => 'SealModelMuseusSBM',
+            'css' => 'model-tab-museus-sbm.css',
+            'background' => 'modelo_certificado_museus_sbm.jpg',
+            'preview' => 'preview-model-museus.png'
+        ];
+    }
+}
---
+++ plugins/SealModelMuseusSBM/printsealrelation.php
@@ -0,0 +1,12 @@
+<div id="certificate-model-museus">
+    <img class ="background" src="<?php $view->asset('img/modelo_certificado_museus.jpg') ?>"/>
+    <p class="certificate-content"><?php echo nl2br($msg) ?></p>
+    <div class="footer">
+        <div class="entity-url">
+            <a  href="<?php echo $relation->owner->getSingleUrl(); ?>"
+                title="<?php echo $relation->owner->name ?>"><?php echo $relation->owner->getSingleUrl(); ?></a>
+        </div>
+        <div class="footer-signatures">
+        </div>
+    </div>
+</div>
