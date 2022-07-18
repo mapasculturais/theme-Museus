a24d72b2bdb6d44bfce14df0cbf276bd6a397a0c
erleibiazzio <erleibiazzio@gmail.com>
2022-07-18 09:29:08 -0300

    Configura plugin LocationPatch como submodulo do projeto

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -8,5 +8,13 @@ return [
         
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        'Fva' =>['namespace'=>'Fva'],
+        "LocationPatch" => [
+            "namespace" => "LocationPatch",
+            "config" => [
+                "enable" => env("LOCATION_PATCH_ENABLE", false),
+                "cutoff" => env("LOCATION_PATCH_CUTOFF", "19800101000001"),
+            ],
+        ],
     ]
 ];
