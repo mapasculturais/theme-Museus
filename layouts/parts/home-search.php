4f53a9f5856f144b7a8d9fb29369c75976fd4347
erleibiazzio <erleibiazzio@gmail.com>
2022-09-13 18:05:14 -0300

    Justifica texto da Home

--- layouts/parts/home-search.php
+++ layouts/parts/home-search.php
@@ -2,7 +2,9 @@
     <?php $this->applyTemplateHook('home-search','begin'); ?>
     <div class="box">
         <h1><?php echo $app->view->renderMarkdown($this->dict('home: title',false)); ?></h1>
-        <p><?php echo $app->view->renderMarkdown($this->dict('home: welcome',false)); ?></p>
+        <div style="text-align: justify;">
+            <p><?php echo $app->view->renderMarkdown($this->dict('home: welcome',false)); ?></p>
+        </div>
         <?php $this->applyTemplateHook('home-search-form','begin'); ?>
         <form id="home-search-form" class="clearfix" ng-non-bindable>
             <?php $this->applyTemplateHook('home-search-form','before'); ?>
