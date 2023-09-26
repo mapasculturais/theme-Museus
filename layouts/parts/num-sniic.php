cfd976fb067d5097c662b4208288e8b067ea0bd7
wolegario40@gmail.com <wolegario49@gmail.com>
2023-09-26 11:34:12 -0300

    Remove template parts não mais ultilizado

--- layouts/parts/num-sniic.php
+++
@@ -1,8 +0,0 @@
-<?php if($this->isEditable() || $entity->num_sniic): ?>
-    <?php $this->applyTemplateHook('num-sniic','before'); ?>
-    <p class="num_sniic">
-        <span class="label">Nº SNIIC:</span> 
-        <span id="num-sniic"><?php echo $entity->num_sniic ? $entity->num_sniic : "Preencha os campos obrigatorios e clique em salvar para gerar"; ?></span>
-    </p>
-    <?php $this->applyTemplateHook('num-sniic','after'); ?>
-<?php endif; ?>
--- layouts/parts/panel/highlighted-message--numsniic.php
+++
@@ -1,4 +0,0 @@
-<br/>
-<?php if($app->user->profile->num_sniic):?>
-<?php \MapasCulturais\i::_e('O Nº SNIIC do seu agente padrão') ?> "<?php echo $app->user->profile->name ?>" <?php \MapasCulturais\i::_e('é') ?> <?php echo $app->user->profile->num_sniic ?>
-<?php endif?>
