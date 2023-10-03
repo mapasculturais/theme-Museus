418d404cbf4b576e1f01f5809ea30426fd8745a3
wolegario40@gmail.com <wolegario49@gmail.com>
2023-10-03 12:01:34 -0300

    Faz criação da telas da edit-visitação  Ref.:#24

--- layouts/parts/museus/tab-edit-visitacao.php
+++ layouts/parts/museus/tab-edit-visitacao.php
@@ -27,20 +27,14 @@ $this->import('
                 <div class="grid-12">
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('O museu encontra-se') ?>" prop="mus_status"></entity-field>
                     <div class="grid-12 col-12">
-                        <p class="col-12"><?php i::_e("Em caso de museu fechado, qual a previsão de abertura? Selecione mês e ano abaixo") ?></p>
-                        <entity-field :entity="entity" classes="col-3" label="<?= i::esc_attr__('Mês') ?>" prop="mus_previsao_abertura_mes"></entity-field>
-                        <entity-field :entity="entity" classes="col-3" label="<?= i::esc_attr__('Ano') ?>" prop="mus_previsao_abertura_ano"></entity-field>
+                        <p class="col-12"><?php i::_e("Em caso de museu fechado, qual a previsão de abertura? Descreva mês e o ano abaixo") ?></p>
+                        <entity-field :entity="entity" classes="col-3" label="<?= i::esc_attr__('Mes/Ano') ?>" prop="mus_previsao_abertura_ano"></entity-field>
                     </div>
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('A entrada do museu é cobrada?') ?>" prop="mus_ingresso_cobrado"></entity-field>
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Informe o valor cobrado para o público geral') ?>" prop="mus_ingresso_valor"></entity-field>
                     <div class="grid-12 col-12">
                         <p class="col-12"><?php i::_e("Dias e horário de funcionamento") ?></p>
-                        <entity-field :entity="entity" classes="col-3" label="<?= i::esc_attr__('Horário inicial') ?>" prop="mus_obs_horario"></entity-field>
-                        <entity-field :entity="entity" classes="col-3" label="<?= i::esc_attr__('Horário final') ?>" prop="mus_obs_horario"></entity-field>
-                    </div>
-                    <div class="grid-12 col-12">
-                        <p class="col-12"><?php i::_e("Dias da semana") ?></p>
-                        <p>Definir os dias não tem o metadados</p>
+                        <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Insira os dias e horários de funcionamento do museu') ?>" prop="mus_obs_horario"></entity-field>
                     </div>
                 </div>
             </template>
@@ -53,13 +47,9 @@ $this->import('
             <template #content>
                 <div class="grid-12">
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('O espaço oferece acessibilidade?') ?>" prop="mus_arquivo_acessoPublico"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Adicionar recursos de acessibilibidade física') ?>" prop="mus_acessibilidade_visual"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Adicionar recursos de acessibilibidade para pessoas com deficiência auditiva e/ou visuais') ?>" prop="mus_acess_visual_auditiva"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Possui recursos de atendimento para estrangeiros?') ?>" prop="mus_servicos_atendimentoEstrangeiros"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Possui sinalização visual em outros idiomas?') ?>" prop="mus_acessibilidade_visual"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Oferece material de divulgação impressa em outros idiomas?') ?>" prop="mus_servicos_atendimentoEstrangeiros"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Oferece áudioguia em outros idiomas?') ?>" prop="mus_servicos_atendimentoEstrangeiros"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Oferece guia/monitor/mediador/orientador/educador que fala outro idioma?') ?>" prop="mus_servicos_atendimentoEstrangeiros"></entity-field>
+                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Adicionar recursos de acessibilibidade física') ?>" prop="mus_acessibilidade_visual"></entity-field>
+                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Adicionar recursos de acessibilibidade para pessoas com deficiência auditiva e/ou visuais') ?>" prop="mus_acess_visual_auditiva"></entity-field>
+                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Possui recursos de atendimento para estrangeiros?') ?>" prop="mus_servicos_atendimentoEstrangeiros"></entity-field>
                 </div>
             </template>
         </mc-card>
@@ -70,12 +60,10 @@ $this->import('
             </template>
             <template #content>
                 <div class="grid-12">
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)Informe as instação básicas e serviços oferecidos pelo museu') ?>" prop="mus_instalacoes"></entity-field>
+                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Informe as instação básicas e serviços oferecidos pelo museu') ?>" prop="mus_instalacoes"></entity-field>
                     <entity-field :entity="entity" classes="col-3" label="<?= i::esc_attr__('Capacidade do teatro/auditótio') ?>" prop="mus_instalacoes_capacidadeAuditorio"></entity-field>
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Possui arquivo histórico?') ?>" prop="mus_arquivo_possui"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('O arquivo histórico está aberto para consulta de usuários externos?') ?>" prop="mus_arquivo_acessoPublico"></entity-field>
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('O museu possui biblioteca?') ?>" prop="mus_biblioteca_possui"></entity-field>
-                    <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('(rever)O arquivo bibliográfico está aberto para consulta de usuários externos?') ?>" prop="mus_biblioteca_possui"></entity-field>
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('O museu possui visita guiada?') ?>" prop="mus_servicos_visitaGuiada"></entity-field>
                     <entity-field :entity="entity" classes="col-12" label="<?= i::esc_attr__('Especifique:') ?>" prop="mus_servicos_visitaGuiada_s"></entity-field>
                 </div>
