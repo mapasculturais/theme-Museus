d1edadf1bf7b396ef6ff1bf89a5a78b4c4f7540d
Carolina Pedrosa <carolinasouza0@gmail.com>
2025-04-03 11:10:32 -0300

    adiciona botão de pontos de memória na homepage

---
+++ layouts/parts/museus/mc-header-menu-memory-point.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\MapasMuseus\Theme $this
+ */
+
+use MapasCulturais\i;
+
+?>
+<div class="mc-header-menu--memory-point">
+    <a href="<?= $app->createUrl('search', 'spaces') ?>" class="mc-header-menu--item space">
+        <span class="icon"> <mc-icon name="space"> </span>
+        <p class="label"> <?php i::_e('Pontos de memória') ?> </p>
+    </a> 
+</div>
+
+
