ad548fd201c5dfe7437ad76b7ab1eb961f47510d
erleibiazzio <erleibiazzio@gmail.com>
2025-04-05 08:55:06 -0300

    Corrige url que carrega os pontos de memoria

--- layouts/parts/museus/mc-header-menu-memory-point.php
+++ layouts/parts/museus/mc-header-menu-memory-point.php
@@ -8,7 +8,7 @@ use MapasCulturais\i;
 
 ?>
 <div class="mc-header-menu--memory-point">
-    <a href="<?= $app->createUrl('search', 'spaces') ?>/?ponto_memoria=true?" class="mc-header-menu--item space">
+    <a href="<?= $app->createUrl('search', 'spaces') ?>?ponto_memoria=true?" class="mc-header-menu--item memory">
         <span class="icon"> <mc-icon name="space"> </span>
         <p class="label"> <?php i::_e('Pontos de memória') ?> </p>
     </a> 
