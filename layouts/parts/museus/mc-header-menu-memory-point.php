2effc651c382c55ab76624f69618d826de497b81
erleibiazzio <erleibiazzio@gmail.com>
2025-04-08 15:04:39 -0300

    Faz com que os menus no topo fiquem com s nomes corretos Museus e pontos de memoria

--- layouts/parts/museus/mc-header-menu-memory-point.php
+++ layouts/parts/museus/mc-header-menu-memory-point.php
@@ -8,6 +8,17 @@
 use MapasCulturais\i;
 
 ?>
+<?php $this->applyTemplateHook('mc-header-menu-museus', 'before') ?>
+<li>
+    <?php $this->applyTemplateHook('mc-header-menu-museus', 'begin') ?>
+    <a href="<?= $app->createUrl('search', 'spaces') ?>" class="mc-header-menu--item museus">
+        <span class="icon"> <mc-icon name="space"> </span>
+        <p class="label"> <?php i::_e('Museus') ?> </p>
+    </a>
+    <?php $this->applyTemplateHook('mc-header-menu-museus', 'end') ?>
+</li>
+<?php $this->applyTemplateHook('mc-header-menu-museus', 'after') ?>
+
 <?php $this->applyTemplateHook('mc-header-menu-memory', 'before') ?>
 <li>
     <?php $this->applyTemplateHook('mc-header-menu-memory', 'begin') ?>
