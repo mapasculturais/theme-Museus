a7d02d3a7635a54900d18e1dc5a3fa20430729b4
leopiccionia <leopiccionia@gmail.com>
2025-11-04 12:17:32 -0300

    Remove comentário

--- layouts/parts/museus/entity-info-fields.php
+++ layouts/parts/museus/entity-info-fields.php
@@ -9,9 +9,8 @@ use MapasCulturais\i;
 
 $this->import('
     entity-field
-    
+
 ');
 ?>
-<small class="col-12">Adicionar props corretas</small>
 <entity-field :entity="entity" classes="col-6 sm:col-6" prop="mus_cod"></entity-field>
 <entity-field :entity="entity" classes="col-6 sm:col-6" prop="num_sniic"></entity-field>
