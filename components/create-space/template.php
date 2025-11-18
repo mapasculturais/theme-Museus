c04f9e3a9e91b570117b0bf1269a59e40e7c34e1
Welington De Lima Olegario <wolegario49@gmail.com>
2025-11-18 18:11:33 -0300

    Remoção dos botões desnecessários para o salvamento do museu.

---
+++ components/create-space/template.php
@@ -0,0 +1,48 @@
+<?php 
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    entity-field 
+    entity-terms
+    mc-link
+    mc-modal 
+'); 
+?>
+<mc-modal :title="modalTitle" classes="create-modal create-space-modal" button-label="<?php i::_e('Criar Espaço')?>"  @open="createEntity()" @close="destroyEntity()">
+    <template v-if="entity && !entity.id" #default>
+        <label><?php i::_e('Crie um espaço com informações básicas')?><br><?php i::_e('e de forma rápida')?></label>
+        <div class="create-modal__fields">
+            <?php $this->part('museus/create-space-fields') ?>
+        </div>
+    </template>
+    
+    <template v-if="entity?.id" #default>
+        <label><?php i::_e('Você pode completar as informações do seu espaço agora ou pode deixar para depois.  ');?></label><br><br>
+        <label><?php i::_e('Para completar e publicar seu novo espaço, acesse a área <b>Rascunhos</b> em <b>Meus Espaços</b> no <b>Painel de Controle</b>.  ');?></label>
+            
+    </template>
+
+    <template #button="modal">
+        <slot :modal="modal"></slot>
+    </template>
+
+    <template v-if="!entity?.id" #actions="modal">
+        <button class="button button--primary" @click="createPublic(modal)"><?php i::_e('Continuar')?></button>
+        <button class="button button--text button--text-del " @click="modal.close()"><?php i::_e('Cancelar')?></button>
+    </template>
+    <template v-if="entity?.id && entity.status==1" #actions="modal">
+        <mc-link :entity="entity" class="button button--primary-outline button--icon"><?php i::_e('Ver Espaço');?></mc-link>
+        <button class="button button--secondarylight button--icon " @click="modal.close()"><?php i::_e('Completar Depois')?></button>
+        <mc-link :entity="entity" route='edit' class="button button--primary button--icon"><?php i::_e('Completar Informações')?></mc-link>
+     </template>
+     <template v-if="entity?.id && entity.status==0" #actions="modal">
+        <mc-link :entity="entity" class="button button--primary-outline button--icon"><?php i::_e('Ver Espaço');?></mc-link>
+        <button class="button button--secondarylight button--icon " @click="modal.close()"><?php i::_e('Completar Depois')?></button>
+        <mc-link :entity="entity" route='edit' class="button button--primary button--icon"><?php i::_e('Completar Informações')?></mc-link>
+    </template>
+</mc-modal>
